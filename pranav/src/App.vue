315bb758e2ea44bf5e60a8852ddf4535d85edf1c
Pranav Sah <pranav.sah@biz2credit.com>
2018-08-21 10:08:50 +0530

    Removed occurance of quicksand font via CDN

--- pranav/src/App.vue
+++ pranav/src/App.vue
@@ -21,7 +21,7 @@ body {
 }
 
 #app {
-  font-family: Quicksand, Helvetica, Arial, sans-serif;
+  font-family: 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
