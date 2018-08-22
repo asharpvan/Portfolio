d8ffe695172e7579476785297a0a4fbe4a712813
Pranav Sah <pranav.sah@biz2credit.com>
2018-08-22 17:10:25 +0530

    Completed projects page

--- pranav/src/components/Projects.vue
+++ pranav/src/components/Projects.vue
@@ -7,14 +7,132 @@
     </header>
     <!-- About Div -->
     <div class="project_section">
-      <h2 class="about_title">About Title</h2>
-      <img class="about_profile_picture" src="../assets/app/page_related/profile_pic.png" alt="Profile Picture" width="200" height="200">
-      <p class="about_details">Hi, My name is Pranav Sah, an alumni of University of Technology, Sydney and an iOS developer with a rich experience of 6 years of working in product as well as service based companies. In past, I was employed by companies working in ecommerce and healthcare sector, where as currently I work for a reputed financial institution. I consider myself to be equally adept at Swift and Objective C. I have no qualms of working as a lone wolf or in a team. I have experience working for a startup as well as for an MNC. Over the years, I have worked on various tools and technologies. Some of them are listed below: IoT, MSAzure, Bluetooth LE, Crashlytics, Bitrise, Backendless, Parse (deprecated), AVFoundation, Core Data, Core Animation, XML, JSON, iTunes Connect, Consuming of RESTful APIs, Sockets, Maps, SQLite, Social Frameworks and Push Notifications among others.</p>
-    </div>
-    <div class="project_section">
-      <h2 class="about_title">About Title</h2>
-      <img class="about_profile_picture" src="../assets/app/page_related/profile_pic.png" alt="Profile Picture" width="200" height="200">
-      <p class="about_details">Hi, My name is Pranav Sah, an alumni of University of Technology, Sydney and an iOS developer with a rich experience of 6 years of working in product as well as service based companies. In past, I was employed by companies working in ecommerce and healthcare sector, where as currently I work for a reputed financial institution. I consider myself to be equally adept at Swift and Objective C. I have no qualms of working as a lone wolf or in a team. I have experience working for a startup as well as for an MNC. Over the years, I have worked on various tools and technologies. Some of them are listed below: IoT, MSAzure, Bluetooth LE, Crashlytics, Bitrise, Backendless, Parse (deprecated), AVFoundation, Core Data, Core Animation, XML, JSON, iTunes Connect, Consuming of RESTful APIs, Sockets, Maps, SQLite, Social Frameworks and Push Notifications among others.</p>
+      <div class="project_header_section">
+        <h2 class="project_name">Sanket Life</h2>
+        <img class="project_logo" src="../assets/app/projects/sanket.png" alt="Sanketlife Icon" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">Sanket Life is an official accompanying app for revolutionary Sanket ECG device, a unique clinical grade health device. It can measure and record not only heart rate but also Sugar, SPO2, Cholesterol and Blood Pressure among others.</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> Agatsa Software Pvt. Ltd.</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2016</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 2</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 30</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, Bluetooth LE, MSAzure, CoreData, CoreAnimation</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/in/app/sanket-life-ecg-stress-fitness/id1030796840?mt=8">Sanket iOS App</a></body>
+        <br>
+        <hr>
+      </div>
+      <div class="project_header_section">
+        <h2 class="project_name">IDayofYoga</h2>
+        <img class="project_logo" src="../assets/app/projects/idayofyoga.jpeg" alt="Profile Picture" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">Yoga is a 5,000-year-old physical, mental and spiritual practice Having its origin in India, Which Aims to transform both body and mind. On December 11 in 2014, the United Nations General Assembly declared June 21 as the International Day of Yoga. The app is a syndicated knowledge destination with geo sensing to give you the list of yoga events in your vicinity and provides for online tutorials as well.</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> CanvasM Technologies Limited</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2015</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 1</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 1</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, SQLite, AFNetworking, AVPlayer</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/us/app/idayofyoga/id1007321086?mt=8">IDayofYoga iOS App</a></body>
+        <br>
+        <hr>
+      </div>
+      <div class="project_header_section">
+        <h2 class="project_name">Mage Sales</h2>
+        <img class="project_logo" src="../assets/app/projects/magesale.jpeg" alt="Magesales Icon" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">With Mage Sales, you have instant access to the most important figure from your Magento eCommerce store - your daily revenue. The app is meant for Magento eCommerce store owners who would like to know the daily revenue figures instantly. The app gives you the daily Order totals and Invoice totals.</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> Cue Blocks Technologies Pvt Ltd</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2015</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 1</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 3</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, Magento REST API, XML</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/in/app/mage-sales/id557146525?mt=8">Mage Sales iOS App</a></body>
+        <br>
+        <hr>
+      </div>
+      <div class="project_header_section">
+        <h2 class="project_name">Roomi</h2>
+        <img class="project_logo" src="../assets/app/projects/roomi.jpeg" alt="Roomi Icon" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">Roomi makes it easy for you to find apartments, rooms, and roommates in major cities including New York, San Francisco, Los Angeles, San Diego, Austin, Chicago, Boston, Washington, D.C., Baltimore, Philadelphia, Seattle, Miami, Houston, Dallas, Orlando, Atlanta, Portland, Denver, Las Vegas, Toronto, Vancouver and London, with more cities coming soon. Using this roommate and apartment finder app, you can look for the most compatible roommates, rooms and apartments for rent.</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> Roomi, Inc.</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2015</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 1</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 1</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, Bluetooth LE, MSAzure, CoreData, CoreAnimation</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/us/app/roomi-roommate-room-finder/id690346626?mt=8">Roomi iOS App</a></body>
+        <br>
+        <hr>
+      </div>
+      <div class="project_header_section">
+        <h2 class="project_name">GeoChirpr</h2>
+        <img class="project_logo" src="../assets/app/projects/geochirpr.jpeg" alt="Geochirpr Icon" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">GeoChirpr allows you to easily find latest tweets within 5 mile to 60 miles radius of any location in the world. You can search specific phrases, trending topics and see who is talking about them within the preferred location.</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> Cue Blocks Technologies Pvt Ltd</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2014</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 1</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 1</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, Twitter Rest API, Maps</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/in/app/geochirpr/id481634204?mt=8">GeoChirpr iOS App</a></body>
+        <br>
+        <hr>
+      </div>
+      <div class="project_header_section">
+        <h2 class="project_name">Memize</h2>
+        <img class="project_logo" src="../assets/app/projects/sanket.png" alt="Memize Icon" width="100" height="100">
+      </div>
+      <div class="project_details_section">
+        <body class="project_synopsis">Memize! is a simple app that allows you to create flashcards – cards with paired information on front and back of each of the cards (e.g. language translations, geographical facts, etc.).</body>
+        <br>
+        <body class="project_created_for"><strong>Created For : </strong> Cue Blocks Technologies Pvt Ltd</body>
+        <br>
+        <body class="project_year_released"><strong>Year Released : </strong> 2013</body>
+        <br>
+        <body class="project_team_strength"><strong>Team Strength : </strong> 1</body>
+        <br>
+        <body class="project_no_of_version"><strong>Total Versions Released : </strong> 1</body>
+        <br>
+        <body class="project_techstack"><strong>Technologies Used : </strong> Xcode, SQLite</body>
+        <br>
+        <body class="project_link"><strong>Link : </strong> <a href="https://itunes.apple.com/in/app/memize/id624509524?mt=8">Memize iOS App</a></body>
+        <br>
+        <hr>
+      </div>
     </div>
   </div>
 </template>
@@ -42,6 +160,28 @@ header {
   width: 95%;
   margin: auto;
   text-align: center;
-  background-color: yellow;
+  /* background-color: yellow; */
 }
+
+.project_header_section {
+  /* background-color: blue; */
+  padding-bottom: 0;
+  margin: auto;
+}
+
+.project_details_section {
+  padding-top: 0;
+  text-align: left;
+  /* background-color: green; */
+}
+.project_logo {
+  padding-bottom: 1em;
+  /* border-radius: 90%; */
+}
+/* .project_description {
+  width: 95%;
+  margin: auto;
+  text-align: left;
+  background-color: pink;
+} */
 </style>
