9b1d704272630d8ded773038a791210c7dfe843e
Pranav Sah <pranav.sah@biz2credit.com>
2018-08-24 11:04:26 +0530

    modifying About me page

--- pranav/src/components/About.vue
+++ pranav/src/components/About.vue
@@ -5,31 +5,42 @@
       <img class="about_dp" src="../assets/app/page_related/profile_pic.png" alt="Profile Picture" width="200" height="200">
       <p>Hi, I'm</p>
       <h1 class='about_top_name'>Pranav!</h1>
+      <p>I am an experienced iOS developer and a budding Full Stack Developer.</p>
+      <p>Charming, kind, free spirited and attitude-prone, I was born in the hills of North India and love Trekking, Hiking and generally being close to nature. Coding is a second love of mine, I love creating solution and helping the world along the way.</p>
+      <p>Passionate Chelsea FC fan and a hobbyist photographer, you may choose to follow me here:</p>
     </div>
-    <div class="about_me">
-      <!-- <h2 class='about_me'>About Me</h2> -->
-      <p>An alumni of University of Technology, Sydney and an iOS developer with a rich experience of 6 years of working in product as well as service based companies. In past, I was employed by companies working in ecommerce and healthcare sector, where as currently I work for a reputed financial institution. I consider myself to be equally adept at Swift and Objective C. I have no qualms of working as a lone wolf or in a team. I have experience working for a startup as well as for an MNC. Over the years, I have worked on various tools and technologies. Some of them are listed below: IoT, MSAzure, Bluetooth LE, Crashlytics, Bitrise, Backendless, Parse (deprecated), AVFoundation, Core Data, Core Animation, XML, JSON, iTunes Connect, Consuming of RESTful APIs, Sockets, Maps, SQLite, Social Frameworks and Push Notifications among others.</p>
-    </div>
+    <br>
+    <hr width="50%">
     <div class="professional_me">
       <h2>People I've worked for</h2>
       <div class="work_exp">
         <h3 class="company">Biz2Credit Info Services Pvt Ltd, India</h3>
-        <p class="designation"><b>Senior Market Analyst</b></p>
-        <p class="duration"><b>March 2015-January 2016</b></p>
+        <p class="designation">Senior Software Engineer</p>
+        <p class="duration">May 2017 - Current</p>
       </div>
       <div class="work_exp">
         <h3 class="company">Agatsa Software Pvt Ltd, India</h3>
-        <p class="description">Worked as a <b>Senior Market Analyst</b> from <b>March 2015-January 2016</b></p>
+        <p class="designation">Research & Development Team Lead</p>
+        <p class="duration">June 2016 - April 2017</p>
       </div>
       <div class="work_exp">
-        <h3 class="company">Cueblocks Technologies Pvt Ltd, India</h3>
-        <p class="description">Worked as a <b>Senior Market Analyst</b> from <b>March 2015-January 2016</b></p>
+        <h3 class="company">Agatsa Software Pvt Ltd, India</h3>
+        <p class="designation">iOS Team Lead</p>
+        <p class="duration">June 2015 - May 2016</p>
       </div>
       <div class="work_exp">
-        <h3 class="company">UG Software Pvt Ltd, India</h3>
-        <p class="description">Worked as a <b>Senior Market Analyst</b> from <b>March 2015-January 2016</b></p>
+        <h3 class="company">CueBlocks Technologies Pvt Ltd, India</h3>
+        <p class="designation">iPhone App Developer</p>
+        <p class="duration">December 2012 - May 2015</p>
+      </div>
+      <div class="work_exp">
+        <h3 class="company">UG Software Technologies Pvt Ltd, India</h3>
+        <p class="designation">Trainee iPhone Developer</p>
+        <p class="duration">August 2012 - November 2012</p>
       </div>
     </div>
+    <br>
+    <hr width="50%">
     <div class="skills_me">
       <h2>Tech stack I dabble with</h2>
       <div class="skills_me_gallery">
@@ -132,20 +143,12 @@ export default {
 .about_dp {
   border-radius: 50%;
 }
-.about_me {
-  width: 95%;
-  margin: auto;
-}
-.about_me p {
-  padding-top: 1em;
-  text-align: left;
-  /* background-color: green; */
-}
 
 /******************** Professional Experience Section */
 .professional_me {
   width: 95%;
   margin: auto;
+  text-align: center;
   /* background-color: green; */
 }
 .work_exp {
@@ -154,24 +157,21 @@ export default {
   padding-bottom: 1em;
   /* background-color: red; */
 }
-.professional_me ul {
-  list-style: none;
-}
 .company {
   margin: auto;
   /* background-color: yellowgreen; */
 }
 .designation {
-  margin: 0;
-  padding: auto 10px;
+  margin: auto;
+  width: 98%;
   /* padding: .5em 1em; */
-  background-color: blue;
+  /* background-color: blue; */
 }
 
 .duration {
-  margin: 0;
-  padding: auto 3em;
-  background-color: blueviolet;
+  margin: auto;
+  width: 98%;
+  /* background-color: blueviolet; */
 }
 
 
@@ -179,6 +179,7 @@ export default {
 .skills_me {
   width: 95%;
   margin: auto;
+  text-align: center;
   /* background-color: green; */
 }
 .skills_me_gallery {
